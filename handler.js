08aa934f1ef8fff2af3be1326618ee883dc6abe2
Shane Hopcroft <shanehopcroft@hotmail.com>
2020-01-07 22:21:06 +1100

    Extracted temperatures from event body

--- handler.js
+++ handler.js
@@ -1,6 +1,21 @@
 'use strict';
 
 module.exports.submitTemperatures = async event => {
+  
+  //console.log(JSON.stringify(event));
+
+  console.log("xxx");
+
+  var body = JSON.parse(event.body);
+  var beerTemperature = body.beerTemperature;
+  console.log(beerTemperature);
+  var roomTemperature = body.roomTemperature;
+  console.log(roomTemperature);
+  var fridgeTemperature = body.fridgeTemperature;
+  console.log(fridgeTemperature);
+
+  console.log("yyy");
+
   return {
     statusCode: 200,
     body: JSON.stringify(
