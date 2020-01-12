d7e74dfa16b10c3f0d4b88f4a14ca11006bb9322
Shane Hopcroft <shanehopcroft@hotmail.com>
2020-01-12 12:24:15 +1100

    Refactoring

--- handler.js
+++ handler.js
@@ -5,20 +5,6 @@ AWS.config.update({region: 'ap-southeast-2'});
 
 module.exports.submitTemperatures = async event => {
   
-  //console.log(JSON.stringify(event));
-
-  console.log("xxx");
-
-  var body = JSON.parse(event.body);
-  var beerTemperature = body.beerTemperature;
-  console.log(beerTemperature);
-  var roomTemperature = body.roomTemperature;
-  console.log(roomTemperature);
-  var fridgeTemperature = body.fridgeTemperature;
-  console.log(fridgeTemperature);
-
-  console.log("yyy");
-
   var currentTimeStamp = new Date();
 //  var currentTimeStamp = new Date().toLocaleString("en-AU", {timeZone: "Australia/Melbourne"})
   var date
@@ -33,39 +19,59 @@ module.exports.submitTemperatures = async event => {
       'TimeStamp' : {S: currentTimeStamp.toISOString()},
     }
   };
+
+  // Parse the temperatures out of the event
+  var body = JSON.parse(event.body);
+
+  var beerTemperature = body.beerTemperature;
+  console.log("Beer temperature: " + beerTemperature);
   if(beerTemperature) {
     params.Item.BeerTemperature = {S: beerTemperature};
   }
+
+  var roomTemperature = body.roomTemperature;
+  console.log("Room temperature: " + roomTemperature);
   if(roomTemperature) {
     params.Item.RoomTemperature = {S: roomTemperature};
   }
+
+  var fridgeTemperature = body.fridgeTemperature;
+  console.log("Fridge temperature: " + fridgeTemperature);
   if(fridgeTemperature) {
     params.Item.FridgeTemperature = {S: fridgeTemperature};
   }
 
+  // Write the data to the database
   await ddb.putItem(params)
     .promise()
     .then(function(data) {
       console.log("Success", data);
+    
+      return {
+        statusCode: 200,
+        body: JSON.stringify(
+          {
+            message: 'Success! Temperatures were submitted successfully!',
+            input: event,
+          },
+          null,
+          2
+        ),
+      };
     })
     .catch(function(err) {
       console.log("Error", err);
+
+      return {
+        statusCode: 500,
+        body: JSON.stringify(
+          {
+            message: 'Error! A problem occured when submitting temperatures.',
+            input: event,
+          },
+          null,
+          2
+        ),
+      };
     });
-
-  console.log("zzz");
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
